42a095f31d64fb45e5478c485878b8aa322b3d2f
miteshvp <mitpatel@redhat.com>
2017-07-18 19:06:25 +0530

    fixed sanity check testing

--- src/sanitycheck.py
+++ src/sanitycheck.py
@@ -10,15 +10,6 @@ logger = logging.getLogger(__name__)
 MAX_DELAY = 20 * 60  # 5 minutes
 
 
-def test_websocket_connection():
-    g = BayesianGraph.instance()
-    result = g.V().count().toList()
-    assert (result is not None)
-    vcount = result[0]
-    assert (vcount >= 0)
-    logger.info("Connection to WebSocket endpoint: SUCCESS")
-
-
 def test_http_connection():
     result = BayesianGraph.execute("g.V().count()")
     code, data = result
@@ -48,17 +39,6 @@ def main():
         except Exception as e:
             # tb = traceback.format_exc()
             logger.info("Connection to HTTP endpoint: FAILED... %s" % e)
-            logger.info("Retrying after %s seconds" % waittime)
-            time.sleep(waittime)
-
-    logger.info ("Connecting to WebSocket...")
-    while time_remaining(start_time, time.time()) > 0:
-        try:
-            test_websocket_connection()
-            break
-        except Exception as e:
-            # tb = traceback.format_exc()
-            logger.info("Connection to WebSocket endpoint: FAILED... %s" % e)
             logger.info("Retrying after %s seconds" % waittime)
             time.sleep(waittime)
 
