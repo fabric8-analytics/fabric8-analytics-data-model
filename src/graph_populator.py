07afeacebf11cbfd80217abba9dce7562f38a744
miteshvp <mitpatel@redhat.com>
2017-07-19 15:44:06 +0530

    Make data importer update selective graph properties

--- src/graph_populator.py
+++ src/graph_populator.py
@@ -1,6 +1,7 @@
 import logging
 import re
 import time
+from datetime import datetime
 
 logger = logging.getLogger(__name__)
 
@@ -13,51 +14,50 @@ class GraphPopulator(object):
         version = input_json.get('version')
         ver_deps_count = str(input_json.get('dependents_count', -1))
         description = input_json.get('analyses', {}).get('metadata', {}).get('details', [{}])[0].get('description', '')
-
-        # Get Code Metrics Details
-        count = 0
-        tot_complexity = 0.0
-        for lang in input_json.get('analyses', {}).get('code_metrics', {}).get('details', {}).get('languages', []):
-            if lang.get('metrics', {}).get('functions', {}).get('average_cyclomatic_complexity'):
-                count += 1
-                tot_complexity += lang['metrics']['functions']['average_cyclomatic_complexity']
-        cm_avg_cyclomatic_complexity = str(tot_complexity / count) if count > 0 else '-1'
-        cm_loc = str(input_json.get('analyses', {}).get('code_metrics', {}).get('summary', {}).get('total_lines', -1))
-        cm_num_files = str(input_json.get('analyses', {})
-                           .get('code_metrics', {}).get('summary', {}).get('total_files', -1))
-
-        # Get downstream details
-        shipped_as_downstream = 'false'
-        if len(input_json.get('analyses', {}).get('redhat_downstream', {})
-                       .get('summary', {}).get('all_rhsm_product_names', [])) > 0:
-            shipped_as_downstream = 'true'
-
         str_version = "g.V().has('pecosystem','" + ecosystem + "').has('pname','" + pkg_name + "')" \
-                      ".has('version','" + version + "').properties('licenses','cve_ids').drop().iterate();"\
+                      ".has('version','" + version + "').properties('licenses','cve_ids').drop().iterate();" \
                       "ver = g.V().has('pecosystem','" + ecosystem + "').has('pname','" + pkg_name + "')" \
                       ".has('version','" + version + "').tryNext().orElseGet{graph.addVertex('pecosystem','" \
                       + ecosystem + "', 'pname','" + pkg_name + "', 'version','" + version + "', " \
                       "'vertex_label', 'Version')};" \
-                      "ver.property('last_updated'," + str(time.time()) + ");" \
-                      "ver.property('shipped_as_downstream'," + shipped_as_downstream + ");" \
-                      "ver.property('description','" + re.sub('[^A-Za-z0-9_ ]', '', description or '').lower() + "');" \
                       "ver.property('dependents_count'," + ver_deps_count + ");" \
-                      "ver.property('cm_num_files'," + cm_num_files + ");" \
-                      "ver.property('cm_avg_cyclomatic_complexity'," + cm_avg_cyclomatic_complexity + ");" \
-                      "ver.property('cm_loc'," + str(cm_loc) + ");"
+                      "ver.property('description','" + re.sub('[^A-Za-z0-9_ ]', '', description or '').lower() + "');" \
+                      "ver.property('last_updated'," + str(time.time()) + ");"
+
+        # Get Code Metrics Details
+        if 'code_metrics' in input_json.get('analyses', {}):
+            count = 0
+            tot_complexity = 0.0
+            for lang in input_json.get('analyses').get('code_metrics').get('details', {}).get('languages', []):
+                if lang.get('metrics', {}).get('functions', {}).get('average_cyclomatic_complexity'):
+                    count += 1
+                    tot_complexity += lang['metrics']['functions']['average_cyclomatic_complexity']
+                cm_avg_cyclomatic_complexity = str(tot_complexity / count) if count > 0 else '-1'
+                cm_loc = str(input_json.get('analyses').get('code_metrics').get('summary', {}).get('total_lines', -1))
+                cm_num_files = str(input_json.get('analyses').get('code_metrics').get('summary', {})
+                                   .get('total_files', -1))
+                str_version += "ver.property('cm_num_files'," + cm_num_files + ");" \
+                               "ver.property('cm_avg_cyclomatic_complexity'," + cm_avg_cyclomatic_complexity + ");" \
+                               "ver.property('cm_loc'," + str(cm_loc) + ");"
+
+        # Get downstream details
+        shipped_as_downstream = 'false'
+        if len(input_json.get('analyses', {}).get('redhat_downstream', {})
+               .get('summary', {}).get('all_rhsm_product_names', [])) > 0:
+            shipped_as_downstream = 'true'
+        str_version += "ver.property('shipped_as_downstream'," + shipped_as_downstream + ");"
 
         # Add license details
-        licenses = input_json.get('analyses', {}).get('source_licenses', {}).get('summary', {}).get('sure_licenses', [])
-        str_lic = " ".join(map(lambda x: "ver.property('licenses', '" + x + "');", licenses))
-        str_version += str_lic
+        if 'source_licenses' in input_json.get('analyses', {}):
+            licenses = input_json.get('analyses').get('source_licenses').get('summary', {}).get('sure_licenses', [])
+            str_version += " ".join(map(lambda x: "ver.property('licenses', '" + x + "');", licenses))
 
         # Add CVE property if it exists
-        cves = []
-        for cve in input_json.get('analyses', {}).get('security_issues', {}).get('details', []):
-            cves.append(cve.get('id') + ":" + str(cve.get('cvss', {}).get('score')))
-        str_cve = " ".join(map(lambda x: "ver.property('cve_ids', '" + x + "');", cves))
-
-        str_version += str_cve
+        if 'security_issues' in input_json.get('analyses', {}):
+            cves = []
+            for cve in input_json.get('analyses', {}).get('security_issues', {}).get('details', []):
+                cves.append(cve.get('id') + ":" + str(cve.get('cvss', {}).get('score')))
+            str_version += " ".join(map(lambda x: "ver.property('cve_ids', '" + x + "');", cves))
 
         return str_version
 
@@ -67,51 +67,77 @@ class GraphPopulator(object):
         ecosystem = input_json.get('ecosystem')
         pkg_name_tokens = re.split('\W+', pkg_name)
 
-        # Get Metadata Details
-        latest_version = input_json.get('latest_version') or ''
-        pkg_deps_count = str(input_json.get('package_info', {}).get('dependents_count', -1))
-        pkg_usage = input_json.get('package_info', {}).get('relative_usage', 'NA')
-
-        # Get Github Details
-        gh_details = input_json.get('analyses', {}).get('github_details', {}).get('details', {})
-        gh_prs_last_year_opened = str(gh_details.get('updated_pull_requests', {}).get('year', {}).get('opened', -1))
-        gh_prs_last_month_opened = str(gh_details.get('updated_pull_requests', {}).get('month', {}).get('opened', -1))
-        gh_prs_last_year_closed = str(gh_details.get('updated_pull_requests', {}).get('year', {}).get('closed', -1))
-        gh_prs_last_month_closed = str(gh_details.get('updated_pull_requests', {}).get('month', {}).get('closed', -1))
-        gh_issues_last_year_opened = str(gh_details.get('updated_issues', {}).get('year', {}).get('opened', -1))
-        gh_issues_last_month_opened = str(gh_details.get('updated_issues', {}).get('month', {}).get('opened', -1))
-        gh_issues_last_year_closed = str(gh_details.get('updated_issues', {}).get('year', {}).get('closed', -1))
-        gh_issues_last_month_closed = str(gh_details.get('updated_issues', {}).get('month', {}).get('closed', -1))
-        gh_forks = str(gh_details.get('forks_count', -1))
-        gh_stargazers = str(gh_details.get('stargazers_count', -1))
-        gh_open_issues_count = str(gh_details.get('open_issues_count', -1))
-        gh_subscribers_count = str(gh_details.get('subscribers_count', -1))
-
-        # Create the query string
         str_package = "pkg = g.V().has('ecosystem','" + ecosystem + "').has('name','" + pkg_name + "').tryNext()" \
                       ".orElseGet{graph.addVertex('ecosystem', '" + ecosystem + "', 'name', '" + pkg_name + "', " \
-                      "'vertex_label', 'Package')};" \
-                      "pkg.property('gh_prs_last_year_opened', " + gh_prs_last_year_opened + ");" \
-                      "pkg.property('gh_prs_last_month_opened', " + gh_prs_last_month_opened + ");" \
-                      "pkg.property('gh_prs_last_year_closed', " + gh_prs_last_year_closed + ");" \
-                      "pkg.property('gh_prs_last_month_closed', " + gh_prs_last_month_closed + ");" \
-                      "pkg.property('gh_issues_last_year_opened', " + gh_issues_last_year_opened + ");" \
-                      "pkg.property('gh_issues_last_month_opened', " + gh_issues_last_month_opened + ");" \
-                      "pkg.property('gh_issues_last_year_closed', " + gh_issues_last_year_closed + ");" \
-                      "pkg.property('gh_issues_last_month_closed', " + gh_issues_last_month_closed + ");" \
-                      "pkg.property('gh_forks', " + gh_forks + ");" \
-                      "pkg.property('gh_stargazers', " + gh_stargazers + ");" \
-                      "pkg.property('gh_open_issues_count', " + gh_open_issues_count + ");" \
-                      "pkg.property('gh_subscribers_count', " + gh_subscribers_count + ");" \
-                      "pkg.property('latest_version', '" + latest_version + "');" \
-                      "pkg.property('package_relative_used', '" + pkg_usage + "');" \
-                      "pkg.property('package_dependents_count', " + pkg_deps_count + ");" \
-                      "pkg.property('last_updated', " + str(time.time()) + ");"
+                      "'vertex_label', 'Package')};"
+
+        # Get Metadata Details
+        if 'metadata' in input_json.get('analyses', {}):
+            latest_version = input_json.get('latest_version') or ''
+            pkg_deps_count = str(input_json.get('package_info', {}).get('dependents_count', -1))
+            pkg_usage = input_json.get('package_info', {}).get('relative_usage', 'NA')
+            str_package += "pkg.property('latest_version', '" + latest_version + "');" \
+                           "pkg.property('package_relative_used', '" + pkg_usage + "');" \
+                           "pkg.property('package_dependents_count', " + pkg_deps_count + ");" \
+                           "pkg.property('last_updated', " + str(time.time()) + ");"
+
+        # Get Github Details
+        if 'github_details' in input_json.get('analyses', {}):
+            gh_details = input_json.get('analyses').get('github_details').get('details', {})
+            gh_prs_last_year_opened = str(gh_details.get('updated_pull_requests', {}).get('year', {}).get('opened', -1))
+            gh_prs_last_month_opened = str(gh_details.get('updated_pull_requests', {}).get('month', {}).get('opened', -1))
+            gh_prs_last_year_closed = str(gh_details.get('updated_pull_requests', {}).get('year', {}).get('closed', -1))
+            gh_prs_last_month_closed = str(gh_details.get('updated_pull_requests', {}).get('month', {}).get('closed', -1))
+            gh_issues_last_year_opened = str(gh_details.get('updated_issues', {}).get('year', {}).get('opened', -1))
+            gh_issues_last_month_opened = str(gh_details.get('updated_issues', {}).get('month', {}).get('opened', -1))
+            gh_issues_last_year_closed = str(gh_details.get('updated_issues', {}).get('year', {}).get('closed', -1))
+            gh_issues_last_month_closed = str(gh_details.get('updated_issues', {}).get('month', {}).get('closed', -1))
+            gh_forks = str(gh_details.get('forks_count', -1))
+            gh_stargazers = str(gh_details.get('stargazers_count', -1))
+            gh_open_issues_count = str(gh_details.get('open_issues_count', -1))
+            gh_subscribers_count = str(gh_details.get('subscribers_count', -1))
+
+            str_package += "pkg.property('gh_prs_last_year_opened', " + gh_prs_last_year_opened + ");" \
+                           "pkg.property('gh_prs_last_month_opened', " + gh_prs_last_month_opened + ");" \
+                           "pkg.property('gh_prs_last_year_closed', " + gh_prs_last_year_closed + ");" \
+                           "pkg.property('gh_prs_last_month_closed', " + gh_prs_last_month_closed + ");" \
+                           "pkg.property('gh_issues_last_year_opened', " + gh_issues_last_year_opened + ");" \
+                           "pkg.property('gh_issues_last_month_opened', " + gh_issues_last_month_opened + ");" \
+                           "pkg.property('gh_issues_last_year_closed', " + gh_issues_last_year_closed + ");" \
+                           "pkg.property('gh_issues_last_month_closed', " + gh_issues_last_month_closed + ");" \
+                           "pkg.property('gh_forks', " + gh_forks + ");" \
+                           "pkg.property('gh_stargazers', " + gh_stargazers + ");" \
+                           "pkg.property('gh_open_issues_count', " + gh_open_issues_count + ");" \
+                           "pkg.property('gh_subscribers_count', " + gh_subscribers_count + ");"
 
         # Add tokens for a package
         for tkn in pkg_name_tokens:
             if tkn:
                 str_package += "pkg.property('tokens', '" + tkn + "');"
+
+        # Get Libraries.io data
+        if 'libraries_io' in input_json.get('analyses', {}):
+            libio_dependents_projects = input_json.get('analyses').get('libraries_io').get('details', {}) \
+                                                  .get('dependents', {}).get('count', -1)
+            libio_dependents_repos = input_json.get('analyses').get('libraries_io').get('details', {}) \
+                                               .get('dependent_repositories', {}).get('count', -1)
+            libio_total_releases = input_json.get('analyses').get('libraries_io').get('details', {}) \
+                                             .get('releases', {}).get('count', -1)
+            libio_latest_release = input_json.get('analyses').get('libraries_io').get('details', {}) \
+                                             .get('releases', {}).get('latest', {}).get('published_at')
+
+            if libio_latest_release is not None:
+                str_package += "pkg.property('libio_latest_release', '" + \
+                               str(time.mktime(datetime.strptime("2017-05-14T16:28:30Z",
+                                                                 '%Y-%m-%dT%H:%M:%SZ').timetuple()))+"');"
+
+            for key, val in input_json.get('analyses').get('libraries_io').get('details', {}) \
+                                      .get('dependent_repositories', {}).get('top', {}).items():
+                str_package += "pkg.property('libio_usedby', '" + key + ":" + val + "');"
+
+            str_package += "pkg.property('libio_dependents_projects', '" + libio_dependents_projects + "');" \
+                           "pkg.property('libio_dependents_repos', '" + libio_dependents_repos + "');" \
+                           "pkg.property('libio_total_releases', '" + libio_total_releases + "');"
 
         return str_package
 
