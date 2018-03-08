8e3f4cbe811e82929ebe0e9768d8232790652499
Michal Srb <michal@redhat.com>
2018-03-08 15:36:54 +0100

    Fix tests and add more

--- test/test_graph_populator.py
+++ test/test_graph_populator.py
@@ -7,10 +7,16 @@ import pytest
 def test_sanitize_text_for_query():
     """Test GraphPopulator._sanitize_text_for_query()."""
     f = GraphPopulator._sanitize_text_for_query
-    assert 'pkg', f('pkg')
-    assert 'desc', f(' desc\n')
-    assert 'ASL\n"2.0"', f('ASL 2.0')
-    assert 'ok', f('[ok]')
-    assert 'ok', f("'ok'")
+    assert 'pkg' == f('pkg')
+    assert 'desc' == f('desc\n')
+    assert 'desc' == f(' desc')
+    assert 'desc' == f(' desc\n')
+    assert 'ASL 2.0' == f('ASL\n"2.0"')
+    assert '[ok]' == f('["ok\']')
+    assert 'ok' == f("'ok'")
     with pytest.raises(ValueError):
         f(100)
+
+
+if __name__ == '__main__':
+    test_sanitize_text_for_query()
