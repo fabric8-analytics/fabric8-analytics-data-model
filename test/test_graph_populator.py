f11fe1c683ce1cc8b98e6f82868e650089e5d7e6
Michal Srb <michal@redhat.com>
2018-03-08 14:10:10 +0100

    Input can be byte or text string
    data-importer still runs on Python 2, therefore text string is actually
    unicode, not str.

--- test/test_graph_populator.py
+++ test/test_graph_populator.py
@@ -1,3 +1,14 @@
-"""Tests for the graph_populator module (to be done)."""
+from graph_populator import GraphPopulator
+import pytest
 
-# TODO: to be implemented
+
+def test_sanitize_text_for_query():
+    """Test GraphPopulator._sanitize_text_for_query()."""
+    f = GraphPopulator._sanitize_text_for_query
+    assert 'pkg', f('pkg')
+    assert 'desc', f(' desc\n')
+    assert 'ASL\n"2.0"', f('ASL 2.0')
+    assert 'ok', f('[ok]')
+    assert 'ok', f("'ok'")
+    with pytest.raises(ValueError):
+        f(100)
