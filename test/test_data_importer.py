e0c46b75de3ae6d50dd3fb2267a567b7dc019d82
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-03-09 13:42:33 +0100

    Fixed function to parse string as integer

--- test/test_data_importer.py
+++ test/test_data_importer.py
@@ -4,19 +4,19 @@ from data_importer import *
 import pytest
 
 
-def test_parse_int_or_none_integer_input():
+def test_parse_int_or_none_for_integer_input():
     """Test the function parse_int_or_none() for integer inputs."""
     assert 0 == parse_int_or_none(0)
     assert 1 == parse_int_or_none(1)
     assert -1 == parse_int_or_none(-1)
 
 
-def test_parse_int_or_none_float_input():
+def test_parse_int_or_none_for_float_input():
     """Test the function parse_int_or_none() for float inputs."""
     assert 0 == parse_int_or_none(0.0)
     assert 1 == parse_int_or_none(1.0)
     assert 1 == parse_int_or_none(1.1)
-    assert 2 == parse_int_or_none(1.9)
+    assert 1 == parse_int_or_none(1.9)
     assert -1 == parse_int_or_none(-1)
 
 
@@ -24,26 +24,34 @@ def test_parse_int_or_none_for_string_input():
     """Test the function parse_int_or_none() for string input."""
     assert 42 == parse_int_or_none("42")
     assert 42 == parse_int_or_none("42.1")
-    assert 42 == parse_int_or_none("41.9")
+    assert 41 == parse_int_or_none("41.9")
     assert -42 == parse_int_or_none("-42")
     assert -42 == parse_int_or_none("-42.1")
-    assert -42 == parse_int_or_none("-41.9")
+    assert -41 == parse_int_or_none("-41.9")
 
 
 def test_parse_int_or_none_for_unicode_string_input():
     """Test the function parse_int_or_none() for Unicode string input."""
     assert 42 == parse_int_or_none(u"42")
     assert 42 == parse_int_or_none(u"42.1")
-    assert 42 == parse_int_or_none(u"41.9")
+    assert 41 == parse_int_or_none(u"41.9")
     assert -42 == parse_int_or_none(u"-42")
     assert -42 == parse_int_or_none(u"-42.1")
-    assert -42 == parse_int_or_none(u"-41.9")
+    assert -41 == parse_int_or_none(u"-41.9")
 
 
 def test_parse_int_or_none_for_invalid_input():
     """Test the function parse_int_or_none() for invalid input."""
     assert parse_int_or_none(None) is None
-    assert parse_int_or_none(True) is None
-    assert parse_int_or_none(False) is None
+    assert parse_int_or_none(True) == 1
+    assert parse_int_or_none(False) == 0
     assert parse_int_or_none([]) is None
     assert parse_int_or_none({}) is None
+
+
+if __name__ == '__main__':
+    test_parse_int_or_none_for_integer_input()
+    test_parse_int_or_none_for_float_input()
+    test_parse_int_or_none_for_string_input()
+    test_parse_int_or_none_for_unicode_string_input()
+    test_parse_int_or_none_for_invalid_input()
