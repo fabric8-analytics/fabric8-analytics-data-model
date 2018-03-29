d68bfb2742fce2a4e2c19dd32ce73d64651c853c
Saleem Ansari <tuxdna@gmail.com>
2018-03-29 11:43:03 +0530

    Add test for pending list api

--- test/test_rest_api.py
+++ test/test_rest_api.py
@@ -4,7 +4,7 @@ import rest_api
 import unittest
 import logging
 import config
-
+import json
 
 logger = logging.getLogger(config.APP_NAME)
 
@@ -27,6 +27,15 @@ class RestApiTestCase(unittest.TestCase):
         response = self.app.get('/api/v1/liveness')
         logger.info(response)
         assert response.status_code == 200
+
+    def test_pending(self):
+        response = self.app.get('/api/v1/pending')
+        logger.info(response)
+        assert response.status_code == 200
+        data = json.loads(response.get_data())
+        logger.info(data)
+        assert 'pending_list' in data
+        assert 'all_counts' in data
 
 
 if __name__ == '__main__':
