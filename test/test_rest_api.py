f3bdb3e22770b250b47cd81b7a152e7793a829db
Saleem Ansari <tuxdna@users.noreply.github.com>
2018-03-29 08:07:59 +0000

    Add tests for rest api (#137)
    * Add support for testing rest api
    
    * Add logger and readiness test
    
    * Add test for liveness api
    
    * Add test for pending list api
    
    * Add test for graph_sync API
    
    * Add test for ingest_to_graph API
    
    * Add correct content type for POST query
    
    * Fix error with decoding JSON data in POST request
    
    * Add more asserts in response data
    
    * Add test for selective_ingest API
    
    * Add more tests
    
    * Check response code and data for empty requests
    
    * Add nonempty test with invalid input

--- test/test_rest_api.py
+++ test/test_rest_api.py
@@ -1,7 +1,120 @@
 """Tests for the rest_api module (to be done)."""
 
-from flask import current_app, url_for
-import pytest
+import rest_api
+import unittest
+import logging
+import config
+import json
+
+logger = logging.getLogger(config.APP_NAME)
+
+
+class RestApiTestCase(unittest.TestCase):
+    """Test cases for Rest API."""
+
+    def setUp(self):
+        """Make the application instance ready for testing."""
+        rest_api.app.testing = True
+        self.app = rest_api.app.test_client()
+
+    def tearDown(self):
+        """Cleanup application instance after testing."""
+        pass
+
+    def test_readiness(self):
+        """Add test for readiness API."""
+        response = self.app.get('/api/v1/readiness')
+        logger.info(response)
+        assert response.status_code == 200
+
+    def test_liveness(self):
+        """Add test for liveness API."""
+        response = self.app.get('/api/v1/liveness')
+        logger.info(response)
+        assert response.status_code == 200
+
+    def test_pending(self):
+        """Add test for pelding graph sync API."""
+        response = self.app.get('/api/v1/pending')
+        logger.info(response)
+        assert response.status_code == 200
+        data = json.loads(response.get_data())
+        logger.info(data)
+        assert 'pending_list' in data
+        assert 'all_counts' in data
+
+    def test_sync_all(self):
+        """Add test for graph sync API."""
+        response = self.app.get('/api/v1/sync_all')
+        logger.info(response)
+        assert response.status_code == 200
+        data = json.loads(response.get_data())
+        logger.info(data)
+        assert 'count_imported_EPVs' in data
+        assert 'epv' in data
+        assert 'message' in data
+        assert data['message'] == 'Nothing to be synced to Graph!'
+
+    def test_ingest_to_graph(self):
+        """Add test for ingest to graph API."""
+        input_data = []
+        response = self.app.post('/api/v1/ingest_to_graph',
+                                 data=json.dumps(input_data),
+                                 headers={'Content-Type': 'application/json'})
+        logger.info(response)
+        assert response.status_code == 200
+        data = response.get_data()
+        logger.info("Returned data")
+        logger.info(data)
+        data = json.loads(response.get_data())
+        logger.info(data)
+        assert 'count_imported_EPVs' in data
+        assert 'epv' in data
+        assert 'message' in data
+        assert data['message'] == 'Nothing to be synced to Graph!'
+
+    def test_selective_ingest_empty(self):
+        """Add test for selective ingest API with empty inputs."""
+        input_data = {}
+        response = self.app.post('/api/v1/selective_ingest',
+                                 data=json.dumps(input_data),
+                                 headers={'Content-Type': 'application/json'})
+        data = json.loads(response.get_data())
+        logger.info(data)
+        assert response.status_code == 400
+        assert 'No Packages provided. Nothing to be ingested' == data['message']
+
+        input_data = {'package_list': []}
+        response = self.app.post('/api/v1/selective_ingest',
+                                 data=json.dumps(input_data),
+                                 headers={'Content-Type': 'application/json'})
+        data = json.loads(response.get_data())
+        logger.info(data)
+        assert response.status_code == 400
+        assert 'No Packages provided. Nothing to be ingested' == data['message']
+
+        input_data = {'package_list': [], 'select_ingest': []}
+        response = self.app.post('/api/v1/selective_ingest',
+                                 data=json.dumps(input_data),
+                                 headers={'Content-Type': 'application/json'})
+        data = json.loads(response.get_data())
+        logger.info(data)
+        assert response.status_code == 400
+        assert 'No Packages provided. Nothing to be ingested' == data['message']
+
+    def test_selective_ingest_nonempty(self):
+        """Add test for selective ingest API with wrong input."""
+        input_data = {
+            'package_list': [{}],
+            'select_ingest': []}
+        response = self.app.post('/api/v1/selective_ingest',
+                                 data=json.dumps(input_data),
+                                 headers={'Content-Type': 'application/json'})
+        data = json.loads(response.get_data())
+        logger.info(data)
+        assert response.status_code == 400
+        assert 'Invalid keys found in input:' in data['message']
+
 
 if __name__ == '__main__':
-    pass
+    unittest.main()
