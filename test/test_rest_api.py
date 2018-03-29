d9f75e5cf78fd16f8efee75945d9177a10d25fbf
Saleem Ansari <tuxdna@gmail.com>
2018-03-29 11:19:33 +0530

    Add support for testing rest api

--- test/test_rest_api.py
+++ test/test_rest_api.py
@@ -2,6 +2,20 @@
 
 from flask import current_app, url_for
 import pytest
+import rest_api
+import unittest
+
+
+class RestApiTestCase(unittest.TestCase):
+
+    def setUp(self):
+        rest_api.app.testing = True
+        self.app = rest_api.app.test_client()
+
+    def tearDown(self):
+        pass
+
 
 if __name__ == '__main__':
-    pass
+    unittest.main()
+
