7e080cf586801cd8a0660c0b9c58cb6341c9fd80
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-04-03 11:38:02 +0200

    Unit test: test_get_source_name

--- test/test_s3_data_source.py
+++ test/test_s3_data_source.py
@@ -3,7 +3,22 @@
 # TODO: to be implemented
 
 from data_source.s3_data_source import S3DataSource
+import config
 import pytest
 
+
+def test_get_source_name():
+    """Test the method get_source_name()."""
+    access_key = config.MINIO_ACCESS_KEY if config.AWS_S3_ACCESS_KEY_ID == "" \
+        else config.AWS_S3_ACCESS_KEY_ID
+    secret_key = config.MINIO_SECRET_KEY if config.AWS_S3_SECRET_ACCESS_KEY == "" \
+        else config.AWS_S3_SECRET_ACCESS_KEY
+
+    s3dataSource = S3DataSource(src_bucket_name=config.AWS_EPV_BUCKET,
+                                access_key=access_key,
+                                secret_key=secret_key)
+    assert s3dataSource.get_source_name() == "S3"
+
+
 if __name__ == '__main__':
-    pass
+    test_get_source_name()
