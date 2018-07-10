5d4fee0fcc6aeafe8ebeb34a1931cea17926adc3
Jaime Melis <jmelis@redhat.com>
2018-07-10 11:13:09 +0200

    Move to Quay.io
    This PR is part of the effort to move to Quay.
    
    This is the list of changes in this PR:
    
    - Pushes to Quay instead of the Devshift registry
    - Uses an image hosted in Quay to build the RHEL based container image
    - Uses the new env-toolkit to load variables from jenkins-env
    - Changes the default path of the image to quay (note that this should not affect production because it is overridden in the saas repo)
    
    A companion PR should have been submitted to the appropriate saas repo to change the staging url from devshift to quay. The PR to the saas repo should be merged before this one.
    https://github.com/openshiftio/saas-analytics/pull/405

--- cico_setup.sh
+++ cico_setup.sh
@@ -1,20 +1,27 @@
 #!/bin/bash -ex
 
-REGISTRY="push.registry.devshift.net"
+REGISTRY="quay.io"
 
 load_jenkins_vars() {
-    if [ -e "jenkins-env" ]; then
-        <jenkins-env \
-          grep -E "(DEVSHIFT_TAG_LEN|DEVSHIFT_USERNAME|DEVSHIFT_PASSWORD|JENKINS_URL|GIT_BRANCH|GIT_COMMIT|BUILD_NUMBER|ghprbSourceBranch|ghprbActualCommit|BUILD_URL|ghprbPullId)=" \
-          | sed 's/^/export /g' \
-          > ~/.jenkins-env
-        source ~/.jenkins-env
+    if [ -e "jenkins-env.json" ]; then
+        eval "$(./env-toolkit load -f jenkins-env.json \
+                DEVSHIFT_TAG_LEN \
+                QUAY_USERNAME \
+                QUAY_PASSWORD \
+                JENKINS_URL \
+                GIT_BRANCH \
+                GIT_COMMIT \
+                BUILD_NUMBER \
+                ghprbSourceBranch \
+                ghprbActualCommit \
+                BUILD_URL \
+                ghprbPullId)"
     fi
 }
 
 docker_login() {
-    if [ -n "${DEVSHIFT_USERNAME}" -a -n "${DEVSHIFT_PASSWORD}" ]; then
-        docker login -u "${DEVSHIFT_USERNAME}" -p "${DEVSHIFT_PASSWORD}" "${REGISTRY}"
+    if [ -n "${QUAY_USERNAME}" -a -n "${QUAY_PASSWORD}" ]; then
+        docker login -u "${QUAY_USERNAME}" -p "${QUAY_PASSWORD}" "${REGISTRY}"
     else
         echo "Could not login, missing credentials for the registry"
         exit 1
@@ -44,14 +51,15 @@ push_image() {
     local image_name
     local image_repository
     local short_commit
+
     image_name=$(make get-image-name)
     image_repository=$(make get-image-repository)
     short_commit=$(git rev-parse --short=7 HEAD)
 
     if [ "$TARGET" = "rhel" ]; then
-        IMAGE_URL="${REGISTRY}/osio-prod/${image_repository}"
+        IMAGE_URL="${REGISTRY}/openshiftio/rhel-bayesian-data-model-importer"
     else
-        IMAGE_URL="${REGISTRY}/${image_repository}"
+        IMAGE_URL="${REGISTRY}/openshiftio/bayesian-data-model-importer"
     fi
 
     if [ -n "${ghprbPullId}" ]; then
