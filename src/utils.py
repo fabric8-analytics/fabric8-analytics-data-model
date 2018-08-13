e0e7a6806006be9146aa3e044a4d687cb5f8ab90
Yusuf Zainee <yzainee@redhat.com>
2018-08-13 17:59:49 +0530

    docstyle

--- src/utils.py
+++ src/utils.py
@@ -1,21 +1,4 @@
-# -*- coding: utf-8 -*-
-
-# Copyright © 2018 Red Hat Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Author: Yusuf Zainee <yzainee@redhat.com>
-#
+"""Utils functions for generic usage."""
 
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
@@ -72,6 +55,7 @@ def execute_gremlin_dsl(payloads):
 
 
 def get_current_version(eco, pkg):
+    """To fetch the latest version and libio latest version."""
     query_str = "g.V()has('ecosystem', eco).has('name',pkg)"
     payload = {
         'gremlin': query_str,
