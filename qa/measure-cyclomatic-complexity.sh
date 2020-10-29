29a46634dd198dae9b80aa8d2e3454cbe9b2f3bc
Meer Sawood <msawood@redhat.com>
2020-10-29 12:44:02 +0530

    Sawood14012 patch 1 (#2)
    * Update measure-maintainability-index.sh
    
    * Update measure-cyclomatic-complexity.sh
    
    * Update detect-dead-code.sh
    
    * Update detect-common-errors.sh
    
    * Update check-docstyle.sh
    
    * Update run-linter.sh

--- qa/measure-cyclomatic-complexity.sh
+++ qa/measure-cyclomatic-complexity.sh
@@ -7,11 +7,15 @@ pushd "${SCRIPT_DIR}/.."
 function prepare_venv() {
     VIRTUALENV=$(which virtualenv)
     if [ $? -eq 1 ]; then
-        # python36 which is in CentOS does not have virtualenv binary
+        # python34 which is in CentOS does not have virtualenv binary
         VIRTUALENV=$(which virtualenv-3)
     fi
-
-    ${VIRTUALENV} -p python3 venv && source venv/bin/activate && python3 "$(which pip3)" install radon==3.0.1
+    if [ $? -eq 1 ]; then
+        # still don't have virtual environment -> use python3 directly
+        python3 -m venv venv && source venv/bin/activate && python3 "$(which pip3)" install radon==3.0.1
+    else
+        ${VIRTUALENV} -p python3 venv && source venv/bin/activate && python3 "$(which pip3)" install radon==3.0.1
+    fi
 }
 
 [ "$NOVENV" == "1" ] || prepare_venv || exit 1
@@ -19,7 +23,6 @@ function prepare_venv() {
 radon cc -e "test/*" -s -a -i venv .
 
 popd
-
 defects="$(radon cc -e "test/*" -s -n D -i venv . | wc -l)"
 if [[ $defects -gt 0 ]]
 then
